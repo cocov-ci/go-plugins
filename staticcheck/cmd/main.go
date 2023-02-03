e676dc96380f941cc5d092205e06aa6b6142785a
Felipe Mariotti <felipe.mtt95@gmail.com>
2023-02-03 17:33:12 -0300

    feat: Implement staticcheck

---
+++ staticcheck/cmd/main.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+	"github.com/cocov-ci/go-plugin-kit/cocov"
+	"github.com/cocov-ci/go-plugins/staticcheck/plugin"
+)
+
+func main() { cocov.Run(plugin.Run) }
---
+++ staticcheck/plugin/run_test.go
@@ -0,0 +1,36 @@
+package plugin
+
+import (
+	"fmt"
+	"github.com/cocov-ci/go-plugins/common"
+	sdkmocks "github.com/cocov-ci/go-plugins/common/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/zap"
+	"os"
+	"testing"
+)
+
+func TestRun(t *testing.T) {
+	wd, err := os.Getwd()
+	require.NoError(t, err)
+
+	parent := common.FindParentDir(t, wd, "go-plugins")
+
+	ctrl := gomock.NewController(t)
+	ctx := sdkmocks.NewMockContext(ctrl)
+
+	ctx.EXPECT().Workdir().Return(parent)
+	l := zap.NewNop()
+
+	issues, err := run(ctx, l)
+	require.NoError(t, err)
+
+	for _, i := range issues {
+		fmt.Println(i)
+		f, err := os.Stat(i.FilePath)
+		require.NoError(t, err)
+		require.False(t, f.IsDir())
+	}
+
+}
